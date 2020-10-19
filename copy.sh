dff53806fb727a5ab60e45417d82eb968d111285
dsych <dmytrosych834@gmail.com>
2020-10-19 11:40:06 -0400

    adding copying script

--- copy.sh
+++ copy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 configDir=$HOME/.config/nvim
-# make sure that there are no outstanding changes in the current repo
 
-# copy new versions of the configs
 git update-index --refresh | wc -l | grep "^0$" &> /dev/null
 
+# make sure that there are no outstanding changes in the current repo
 if [ $? -ne 0 ] then
         echo "There are outstanding changes. Commit them first."
         exit 1
 fi
 
+# copy new versions of the configs
 cp $configDir/init.nvim $configDir/coc-settings.json .
 
