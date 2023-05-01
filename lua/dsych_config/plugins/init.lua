92a6ab139f01686fed81fc68764f95d9819f586b
Dmytro Sych <dsych@amazon.com>
2023-05-01 11:30:10 -0400

    Migrate to neotree, since nvim-tree is too buggy

--- lua/dsych_config/plugins/init.lua
+++ lua/dsych_config/plugins/init.lua
@@ -326,67 +326,190 @@ return require("packer").startup(function(use)
 
 	-- file explorer {{{
 	use({
-		"kyazdani42/nvim-tree.lua",
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
 		requires = {
-            "nvim-lua/plenary.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
 		},
 		config = function()
 			local map_key = require("dsych_config.utils").map_key
-			map_key("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
-			map_key("n", "<leader>ef", "<cmd>NvimTreeFindFile<cr>")
+			map_key("n", "<leader>e", "<cmd>Neotree toggle=true source=filesystem<cr>")
+			map_key("n", "<leader>ef", "<cmd>Neotree reveal=true toggle=true source=filesystem<cr>")
 
-			vim.g.nvim_tree_auto_ignore_ft = { "startify", "dashboard" } --empty by default, don't auto open tree on specific filetypes.
+			-- Unless you are still migrating, remove the deprecated commands from v1.x
+			vim.g.neo_tree_remove_legacy_commands = true
 
-			require("nvim-tree").setup({
-				view = {
+			require("neo-tree").setup({
+				close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+				popup_border_style = "rounded",
+				enable_git_status = true,
+				enable_diagnostics = true,
+				open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+				sort_case_insensitive = true, -- used when sorting files and directories in the tree
+				default_component_configs = {
+					container = {
+						enable_character_fade = true,
+					},
+					indent = {
+						indent_size = 2,
+						padding = 1, -- extra padding on left hand side
+						-- indent guides
+						with_markers = true,
+						indent_marker = "│",
+						last_indent_marker = "└",
+						highlight = "NeoTreeIndentMarker",
+						-- expander config, needed for nesting files
+						with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+						expander_collapsed = "",
+						expander_expanded = "",
+						expander_highlight = "NeoTreeExpander",
+					},
+					icon = {
+						folder_closed = "",
+						folder_open = "",
+						folder_empty = "ﰊ",
+						-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+						-- then these will never be used.
+						default = "*",
+						highlight = "NeoTreeFileIcon",
+					},
+					modified = {
+						symbol = "[+]",
+						highlight = "NeoTreeModified",
+					},
+					name = {
+						trailing_slash = false,
+						use_git_status_colors = true,
+						highlight = "NeoTreeFileName",
+					},
+					git_status = {
+						symbols = {
+							-- Change type
+							added = "✚",
+							modified = "",
+							deleted = "✖", -- this can only be used in the git_status source
+							renamed = "", -- this can only be used in the git_status source
+							-- Status type
+							untracked = "",
+							ignored = "",
+							unstaged = "",
+							staged = "",
+							conflict = "",
+						},
+					},
+				},
+				window = {
+					position = "left",
 					width = 65,
-					number = true,
-					relativenumber = true,
-				},
-
-				renderer = {
-                    add_trailing = true,
-                    group_empty = true,
-					indent_markers = {
-						enable = true,
+					mapping_options = {
+						noremap = true,
+						nowait = true,
 					},
-                    special_files = {
-                        ["README.md"] = true,
-                        Makefile = true,
-                        MAKEFILE = true,
-                        Config = true,
-                        ["build.gradle"] = true,
-                        [".vimspector.json"] = true,
-                    }
-				},
-
-				-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-				update_cwd = true,
-
-				-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-				update_focused_file = {
-					enable = true,
-				},
-
-				filters = {
-					custom = { ".git", ".cache" },
-				},
-
-				actions = {
-					open_file = {
-						quit_on_open = true,
+					mappings = {
+						["<space>"] = {
+							"toggle_node",
+							nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+						},
+						["<2-LeftMouse>"] = "open",
+						["<cr>"] = "open",
+						["o"] = "open",
+						["<esc>"] = "revert_preview",
+						["P"] = { "toggle_preview", config = { use_float = true } },
+						["l"] = "focus_preview",
+						["s"] = "split_with_window_picker",
+						["v"] = "vsplit_with_window_picker",
+						["t"] = "open_tabnew",
+						-- ["<cr>"] = "open_drop",
+						-- ["t"] = "open_tab_drop",
+						["w"] = "open_with_window_picker",
+						["C"] = "close_node",
+						["z"] = "close_all_nodes",
+						["Z"] = "expand_all_nodes",
+						["a"] = {
+							"add",
+							-- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+							-- some commands may take optional config options, see `:h neo-tree-mappings` for details
+							config = {
+								show_path = "relative", -- "none", "relative", "absolute"
+							},
+						},
+						["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+						["d"] = "delete",
+						["r"] = "rename",
+						["y"] = "copy_to_clipboard",
+						["x"] = "cut_to_clipboard",
+						["p"] = "paste_from_clipboard",
+						["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+						-- ["c"] = {
+						--  "copy",
+						--  config = {
+						--    show_path = "none" -- "none", "relative", "absolute"
+						--  }
+						--}
+						["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+						["q"] = "close_window",
+						["R"] = "refresh",
+						["?"] = "show_help",
+						["<"] = "prev_source",
+						[">"] = "next_source",
 					},
 				},
-
-				diagnostics = {
-					enable = false,
-					show_on_dirs = true,
-					icons = {
-						hint = "",
-						info = "",
-						warning = "",
-						error = "",
+				nesting_rules = {},
+				filesystem = {
+					filtered_items = {
+						visible = false, -- when true, they will just be displayed differently than normal items
+						hide_dotfiles = true,
+						hide_gitignored = true,
+						hide_hidden = true, -- only works on Windows for hidden files/directories
+						hide_by_name = {
+							--"node_modules"
+						},
+						hide_by_pattern = { -- uses glob style patterns
+							--"*.meta",
+							--"*/src/*/tsconfig.json",
+						},
+						always_show = { -- remains visible even if other settings would normally hide it
+							--".gitignored",
+						},
+						never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+							--".DS_Store",
+							--"thumbs.db"
+						},
+						never_show_by_pattern = { -- uses glob style patterns
+							--".null-ls_*",
+						},
 					},
+					follow_current_file = true, -- This will find and focus the file in the active buffer every
+					-- time the current file is changed while the tree is open.
+					group_empty_dirs = true, -- when true, empty folders will be grouped together
+					hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+					use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+					-- instead of relying on nvim autocmd events.
+					window = {
+						mappings = {
+							["<bs>"] = "navigate_up",
+							["."] = "set_root",
+							["H"] = "toggle_hidden",
+							["/"] = "fuzzy_finder",
+							["D"] = "fuzzy_finder_directory",
+							["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+							-- ["D"] = "fuzzy_sorter_directory",
+							["f"] = "filter_on_submit",
+							["<c-x>"] = "clear_filter",
+							["[g"] = "prev_git_modified",
+							["]g"] = "next_git_modified",
+						},
+						fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+							["<down>"] = "move_cursor_down",
+							["<C-n>"] = "move_cursor_down",
+							["<up>"] = "move_cursor_up",
+							["<C-p>"] = "move_cursor_up",
+						},
+					},
+
+					commands = {}, -- Add a custom command or override a global one using the same function name
 				},
 			})
 		end,
