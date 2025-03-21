ab55faa9db8c7ccaabd503c14b608533e675bb5a
Dmytro Sych <dsych@amazon.com>
2025-03-21 19:26:56 +0000

    Add 2 ollama flavours based on url for avant.nvim

--- lua/dsych_config/plugins/ollama.lua
+++ lua/dsych_config/plugins/ollama.lua
@@ -1,32 +1,88 @@
 return {
-  "nomnivore/ollama.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-
-  -- All the user commands added by the plugin
-  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-  keys = {
-    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
-    {
-      "<leader>ll",
-      ":<c-u>lua require('ollama').prompt()<cr>",
-      desc = "ollama prompt",
-      mode = { "n", "v" },
-    },
-
-    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
-    {
-      "<leader>lg",
-      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-      desc = "ollama Generate Code",
-      mode = { "n", "v" },
-    },
-  },
-
-  ---@type Ollama.Config
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    model = "llama3.1:latest"
-  }
+    provider = "ollama",
+    auto_suggestions_provider = "ollama",
+    mappings = {
+      ask = "<leader>ma",
+      edit = "<leader>me",
+      refresh = "<leader>mr",
+      focus = "<leader>mf",
+      toggle = {
+        default = "<leader>mt",
+        debug = "<leader>md",
+        hint = "<leader>mh",
+        suggestion = "<leader>ms",
+        repomap = "<leader>mR",
+      },
+      files = {
+        add_current = "<leader>mc", -- Add current buffer to selected files
+      },
+    },
+    ollama = {
+      endpoint = "http://127.0.0.1:11435",
+      -- model = "qwen2.5-coder",
+      -- model = "llama3.3",
+      -- model = "codellama:13b",
+      -- model = "deepseek-coder-v2",
+      model = "gemma3:12b",
+      disable_tools = true,
+      options = {
+        num_ctx = 8192,
+        temperature = 0,
+      },
+      stream = true,
+    },
+    vendors = {
+      ollama_dev_desk = {
+        __inherited_from = "openai",
+        api_key_name = "",
+        endpoint = "http://127.0.0.1:11434/v1",
+        -- model = "qwen2.5-coder",
+        model = "llama3.2",
+        -- model = "codellama:13b",
+        -- model = "deepseek-coder-v2",
+        -- model = "codegemma",
+        disable_tools = true
+      },
+    },
+    behaviour = {
+      auto_set_keymaps = true
+    },
+    file_selector = {
+      provider = "telescope"
+    },
+    hints = { enabled = false }
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    {
+      "stevearc/dressing.nvim",
+      opts = {
+        select = {
+          enabled = true,
+          telescope = require "telescope.themes".get_dropdown({
+            previewer = false,
+            layout_config = {
+              width = function(_, max_columns, _)
+                return math.floor(max_columns * 0.65)
+              end
+            }
+          })
+
+        }
+      }
+    },
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp",
+    "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+  },
 }
