c74992b99adacaf37716631615884499c89aeea2
Dmytro Sych <dsych@amazon.com>
2025-07-15 02:41:15 +0000

    Add signature help when typing

--- lua/dsych_config/plugins/init.lua
+++ lua/dsych_config/plugins/init.lua
@@ -158,6 +158,21 @@ return {
 			})
 		end,
 	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "InsertEnter",
+		opts = {
+			bind = true,
+			handler_opts = {
+				border = "rounded"
+			}
+		},
+		hint_prefix = {
+			above = "↙ ", -- when the hint is on the line above the current line
+			current = "← ", -- when the hint is on the same line
+			below = "↖ " -- when the hint is on the line below the current line
+		}
+	},
 	-- }}}
 
 	-- language server {{{
