8c8dda52a0e819f435415a9ca1eec076a2a52b82
Dmytro Sych <dsych@amazon.com>
2025-07-15 02:37:14 +0000

    Fix call to deprecated Mason method for resolving tool path

--- lua/dsych_config/utils/mason.lua
+++ lua/dsych_config/utils/mason.lua
@@ -3,7 +3,8 @@ local M = {}
 M.get_package_path_with_fallback = function (mason_package_name, mason_suffix, fallback_path)
     local package_installed, package_installation = pcall(require"mason-registry".get_package, mason_package_name)
     if package_installed then
-        return vim.fn.glob(package_installation:get_install_path() .. mason_suffix)
+        return vim.fn.expand("$MASON/packages/" .. mason_package_name) .. mason_suffix
+        -- return vim.fn.glob(package_installation:get_install_path() .. mason_suffix)
     else
         return fallback_path and vim.fn.glob(fallback_path) or ""
     end
